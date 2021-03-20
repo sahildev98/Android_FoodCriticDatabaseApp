383cf6d49c0f3a7ea7b5dafedef79963ae8ca0f1
Sahil Ahmed <sahil.98@hotmail.com>
2021-03-20 19:47:08 +0000

    Implement fragment transaction in MainActivity, to display in MainActivity.xml.

--- app/src/main/java/com/example/myfoodcriticapp/MainActivity.java
+++ app/src/main/java/com/example/myfoodcriticapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.myfoodcriticapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentTransaction fragmentTransact = getSupportFragmentManager().beginTransaction();
+        fragmentTransact.add(R.id.fragment_container_view, FragmentActivity.class,null);
+        fragmentTransact.commit();
     }
 }
