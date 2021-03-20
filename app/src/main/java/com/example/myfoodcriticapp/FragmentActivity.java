da1e3c0b80c52ecef1b4720c39776b3ee64100cd
Sahil Ahmed <sahil.98@hotmail.com>
2021-03-20 19:44:20 +0000

    Implement fragment linearlayout with + onView method.

--- app/src/main/java/com/example/myfoodcriticapp/FragmentActivity.java
+++ app/src/main/java/com/example/myfoodcriticapp/FragmentActivity.java
@@ -2,14 +2,24 @@ package com.example.myfoodcriticapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 
 public class FragmentActivity extends Fragment {
+
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        LinearLayout layout = view.findViewById(R.id.fragment_layout);
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
