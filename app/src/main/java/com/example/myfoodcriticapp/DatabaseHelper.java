0400a0cae59fc9dcfbac6d70d766065101f16964
Sahil Ahmed <sahil.98@hotmail.com>
2021-03-22 14:26:11 +0000

    Create/Open myFoodCritic Database. Insert food table with appropriate field names and data types.

--- app/src/main/java/com/example/myfoodcriticapp/DatabaseHelper.java
+++ app/src/main/java/com/example/myfoodcriticapp/DatabaseHelper.java
@@ -1,4 +1,30 @@
 package com.example.myfoodcriticapp;
 
-public class DatabaseHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public DatabaseHelper (Context context) { super(context, "myFoodCriticDB.db", null, 1); }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String food_query = "CREATE TABLE IF NOT EXISTS tblFood (" +
+                "FoodID int PRIMARY KEY AUTOINCREMENT, "+
+                "foodName text,"+
+                "price real,"+
+                "foodDescription text"+
+                ")";
+                db.execSQL(food_query);
+
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("Drop TABLE IF EXISTS tblFood");
+        onCreate(db);
+    }
 }
