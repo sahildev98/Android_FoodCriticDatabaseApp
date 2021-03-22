bc33db243b972cbba714c7715fa5babd1773849d
Sahil Ahmed <sahil.98@hotmail.com>
2021-03-22 19:49:33 +0000

    Created tables for restaurant and ratings with appropriate datatypes. Also added foreign keys in tblfood and tblratings.

--- app/src/main/java/com/example/myfoodcriticapp/DatabaseHelper.java
+++ app/src/main/java/com/example/myfoodcriticapp/DatabaseHelper.java
@@ -4,27 +4,47 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import androidx.annotation.Nullable;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
     public DatabaseHelper (Context context) { super(context, "myFoodCriticDB.db", null, 1); }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String food_query = "CREATE TABLE IF NOT EXISTS tblFood (" +
+        String restaurant_Query = "CREATE TABLE IF NOT EXISTS tblRestaurant (" +
+                "RestaurantID int PRIMARY KEY AUTOINCREMENT,"+
+                "CustomerID int, "+
+                "restaurantName text,"+
+                "Address text,"+
+                "phoneNum int"+
+                ")";
+                db.execSQL(restaurant_Query);
+
+        String food_Query = "CREATE TABLE IF NOT EXISTS tblFood (" +
                 "FoodID int PRIMARY KEY AUTOINCREMENT, "+
+                "PlaceID int,"+
                 "foodName text,"+
                 "price real,"+
-                "foodDescription text"+
+                "FOREIGN KEY (PlaceID) REFERENCES PlaceID(RestaurantID)"+
                 ")";
-                db.execSQL(food_query);
+                db.execSQL(food_Query);
 
 
+        String ratings_query = "CREATE TABLE IF NOT EXISTS tblRatings (" +
+                "RatingsID int PRIMARY KEY AUTOINCREMENT,"+
+                "locationID int,"+
+                "Ratings numeric,"+
+                "food_description text,"+
+                "FOREIGN KEY (LocationID) REFERENCES LocationID(RestaurantID)"+
+                ")";
+                db.execSQL(ratings_query);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("Drop TABLE IF EXISTS tblRestaurant");
+        onCreate(db);
         db.execSQL("Drop TABLE IF EXISTS tblFood");
         onCreate(db);
+        db.execSQL("Drop TABLE IF EXISTS tblRatings");
     }
 }
