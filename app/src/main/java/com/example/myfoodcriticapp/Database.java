3695e6b6655d50b46667b23267bfc4ba8f54e46b
Sahil Ahmed <sahil.98@hotmail.com>
2021-03-27 19:45:49 +0000

    Added a public method to read/write to the database so that data can be inserted.

--- app/src/main/java/com/example/myfoodcriticapp/Database.java
+++ app/src/main/java/com/example/myfoodcriticapp/Database.java
@@ -1,9 +1,17 @@
 package com.example.myfoodcriticapp;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 
-public class Database extends DatabaseHelper{
+public class Database extends DatabaseHelper {
     public Database(Context context) {
         super(context);
+    }
+
+    public long addRestaurant(ContentValues values) {
+        SQLiteDatabase db = getWritableDatabase();
+        return db.insert("tblRestaurant", null, values);
     }
 }
