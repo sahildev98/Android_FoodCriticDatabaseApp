096e0a8d2aa41aa7694c2ff37efb1f7fb557f006
Sahil Ahmed <sahil.98@hotmail.com>
2021-03-27 20:12:19 +0000

    Added a output method for restaurant, using a cursor to return a query that has a set of random read/write data.

--- app/src/main/java/com/example/myfoodcriticapp/Database.java
+++ app/src/main/java/com/example/myfoodcriticapp/Database.java
@@ -14,4 +14,19 @@ public class Database extends DatabaseHelper {
         SQLiteDatabase db = getWritableDatabase();
         return db.insert("tblRestaurant", null, values);
     }
+
+    public Cursor outputRestaurantTable() {
+        SQLiteDatabase db  = this.getWritableDatabase();
+        String table = "tblRestaurant";
+        String [] columns = {"restaurantName", "address, phoneNum"};
+        String selection = "";
+        String  [] selectionArgs = {};
+        String groupBy = null;
+        String having = null;
+        String orderBy = "restaurantName DESC";
+        String limit = "100";
+
+        Cursor cursor = db.query(table,columns,selection,selectionArgs,groupBy,having,orderBy,limit);
+        return cursor;
+    }
 }
