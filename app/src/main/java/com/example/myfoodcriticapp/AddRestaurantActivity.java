93ee3579a8627eb6d06ee0f819a7fb1fe763021b
Sahil Ahmed <sahil.98@hotmail.com>
2021-03-28 23:13:42 +0100

    Declared and initialised EditText views to insert data.

--- app/src/main/java/com/example/myfoodcriticapp/AddRestaurantActivity.java
+++ app/src/main/java/com/example/myfoodcriticapp/AddRestaurantActivity.java
@@ -4,9 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class AddRestaurantActivity extends AppCompatActivity {
-
+EditText restaurant_Name, restaurant_Address, restaurant_Phone;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,5 +15,10 @@ public class AddRestaurantActivity extends AppCompatActivity {
         FragmentTransaction fragmentTransact = getSupportFragmentManager().beginTransaction();
         fragmentTransact.add(R.id.fragment_container_view, FragmentActivity.class,null);
         fragmentTransact.commit();
+
+        restaurant_Name = findViewById(R.id.restaurant_Textbox);
+        restaurant_Address = findViewById(R.id.address_Textbox);
+        restaurant_Phone = findViewById(R.id.phone_Textbox);
+
     }
 }
