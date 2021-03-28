4a8c64369482b96466c5b84342af4b4a2d39a910
Sahil Ahmed <sahil.98@hotmail.com>
2021-03-28 21:12:48 +0100

    Altered MainActivity for start btn, to  direct users to the AddRestaurantActivity.

--- app/src/main/java/com/example/myfoodcriticapp/MainActivity.java
+++ app/src/main/java/com/example/myfoodcriticapp/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, AddFoodActivity.class));
+                startActivity(new Intent(MainActivity.this, AddRestaurantActivity.class));
             }
         });
 
