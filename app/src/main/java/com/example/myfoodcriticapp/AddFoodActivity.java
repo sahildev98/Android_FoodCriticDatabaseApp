8e9f1ab0a99ce14b38f71c415c48d2caee9c1811
Sahil Ahmed <sahil.98@hotmail.com>
2021-03-29 16:58:20 +0100

    Implemented a Long ID to receive data from AddRestaurantActivity. This will represent as a foreign key.

--- app/src/main/java/com/example/myfoodcriticapp/AddFoodActivity.java
+++ app/src/main/java/com/example/myfoodcriticapp/AddFoodActivity.java
@@ -2,6 +2,7 @@ package com.example.myfoodcriticapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.os.Bundle;
 
 public class AddFoodActivity extends AppCompatActivity {
@@ -10,5 +11,13 @@ public class AddFoodActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_food);
+        Long id = getIntent().getLongExtra("ID",0);
+        Database db = new Database(getApplicationContext());
+        Cursor tblRestaurant = db.outputRestaurantTable();
+        tblRestaurant.moveToFirst();
+
+
+
+
     }
 }
