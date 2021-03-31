47dbef2c77f6d8d07858710680986aa15adb5f00
Sahil Ahmed <sahil.98@hotmail.com>
2021-04-01 00:57:39 +0100

    Modified Ids to follow proper naming conventions. JavadocComment/comments are implemented for explanation of code.

--- app/src/main/java/com/example/myfoodcriticapp/AddRestaurantActivity.java
+++ app/src/main/java/com/example/myfoodcriticapp/AddRestaurantActivity.java
@@ -5,53 +5,57 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.content.ContentValues;
 import android.content.Intent;
-import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class AddRestaurantActivity extends AppCompatActivity {
-EditText restaurant_Name, restaurant_Address, restaurant_Phone;
-Button continue_btn;
+EditText restaurantName, restaurantAddress, restaurantPhone;
+Button continueBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_restaurant);
+        /**
+         * @return the fragment manager that is associated with this activity. Fragment is then
+         * initialised with a beginTransaction method.
+         */
         FragmentTransaction fragmentTransact = getSupportFragmentManager().beginTransaction();
         fragmentTransact.add(R.id.fragment_container_view, FragmentActivity.class,null);
         fragmentTransact.commit();
 
-        restaurant_Name = findViewById(R.id.restaurant_Textbox);
-        restaurant_Address = findViewById(R.id.address_Textbox);
-        restaurant_Phone = findViewById(R.id.phone_Textbox);
-        continue_btn = findViewById(R.id.continue_Btn);
+        // The following EditText views and a button are initialised to the corresponding ids set in activity_add_restaurant xml.
+        restaurantName = findViewById(R.id.nameTextbox);
+        restaurantAddress = findViewById(R.id.addressTextbox);
+        restaurantPhone = findViewById(R.id.phoneTextbox);
+        continueBtn = findViewById(R.id.continueBtn);
 
-        continue_btn.setOnClickListener(new View.OnClickListener() {
+        continueBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-            /* Values that will be inserted in to the database with String Restaurant name
-            address and phone number. */
-                String restaurantNameValue = restaurant_Name.getText().toString();
-                String restaurantAddressValue = restaurant_Address.getText().toString();
-                String restaurantPhoneNumValue = restaurant_Phone.getText().toString();
+            /* Values that will be inserted into the database with the following columns: name
+            address and phone number passed in as Strings.  */
+                String restaurantNameValue = restaurantName.getText().toString();
+                String restaurantAddressValue = restaurantAddress.getText().toString();
+                String restaurantPhoneNumValue = restaurantPhone.getText().toString();
+                // ContentValues class is used for containing data that can be passed onto.
                 ContentValues values = new ContentValues();
                 values.put("name", restaurantNameValue);
                 values.put("address", restaurantAddressValue);
                 values.put("phone", restaurantPhoneNumValue);
                 // returns the application object context in order to return data
                 Database db = new Database(getApplicationContext());
-                 /* Calls the addRestaurant() method in database class and save data +
-                 move to the next activity called AddFoodActivity.*/
+                 /* Calls the addRestaurant() method in database class and save data.
+                    Long instance is created
+                    to parse the primary key to the next activity as a foreign key use.
+                    The startActivity occurs to to initiate the next activity called AddFoodActivity.
+                 */
                 long restaurantID = db.addRestaurant(values);
                 Intent intent = new Intent(AddRestaurantActivity.this,AddFoodActivity.class);
                 intent.putExtra("ID",restaurantID);
                 startActivity(intent);
-
-
             }
-
         });
-
     }
 }
