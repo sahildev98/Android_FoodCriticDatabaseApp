d315b2290d694cb2c502bd3f296bb609ce4559e1
Sahil Ahmed <sahil.98@hotmail.com>
2021-04-01 00:33:22 +0100

    Declared javadoc comments. Implementation of fragment manager, to initiate fragment activity into AddFoodActivity.

--- app/src/main/java/com/example/myfoodcriticapp/AddFoodActivity.java
+++ app/src/main/java/com/example/myfoodcriticapp/AddFoodActivity.java
@@ -1,6 +1,7 @@
 package com.example.myfoodcriticapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.database.Cursor;
 import android.os.Bundle;
@@ -11,10 +12,20 @@ public class AddFoodActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_food);
+        /**
+         * @return the fragment manager that is associated with this activity. Fragment is then
+         * initialised with a beginTransaction method.
+         */
+        FragmentTransaction fragmentTransact = getSupportFragmentManager().beginTransaction();
+        fragmentTransact.add(R.id.fragment_container_view, FragmentActivity.class,null);
+        fragmentTransact.commit();
+        /**
+         * retrieves intent data from from previous activity. This ID represents as a foreign key to the
+         * table restaurant ID
+         */
         Long id = getIntent().getLongExtra("ID",0);
         Database db = new Database(getApplicationContext());
-        Cursor tblRestaurant = db.outputRestaurantTable();
-        tblRestaurant.moveToFirst();
+
 
 
 
