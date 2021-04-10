f8a15af25f09c4700dc2f0a1895f80d19c6472a5
Sahil Ahmed <sahil.98@hotmail.com>
2021-04-10 21:00:58 +0100

    Button has a setOnClickListner implemented.

--- app/src/main/java/com/example/myfoodcriticapp/AddFoodActivity.java
+++ app/src/main/java/com/example/myfoodcriticapp/AddFoodActivity.java
@@ -5,6 +5,7 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -34,6 +35,12 @@ Button continueBtn;
         foodDescription = findViewById(R.id.descriptionTextbox);
         continueBtn = findViewById(R.id.continueBtn);
 
+        continueBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
 
 
