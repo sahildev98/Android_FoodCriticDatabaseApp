43f0edc5f7b1ed06e81e5e64a60cc3ec25dd9aed
Sahil Ahmed <sahil.98@hotmail.com>
2021-04-16 01:49:13 +0100

    Created new OutputFoodCrticActivity to display all the data that has been entered in each activity.

---
+++ app/src/main/java/com/example/myfoodcriticapp/OutputFoodCrticActivity.java
@@ -0,0 +1,14 @@
+package com.example.myfoodcriticapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OutputFoodCrticActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_output_food_crtic);
+    }
+}
