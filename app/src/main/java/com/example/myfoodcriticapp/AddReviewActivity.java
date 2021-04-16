df15b20b41f40afe6cdbbbc1278dee84fa9ecf10
Sahil Ahmed <sahil.98@hotmail.com>
2021-04-16 02:12:21 +0100

    Modified String foodname to storeFoodNae and implemented TextView to parse data in a view.

--- app/src/main/java/com/example/myfoodcriticapp/AddReviewActivity.java
+++ app/src/main/java/com/example/myfoodcriticapp/AddReviewActivity.java
@@ -3,14 +3,20 @@ package com.example.myfoodcriticapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.ContentValues;
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RatingBar;
+import android.widget.TextView;
 
 public class AddReviewActivity extends AppCompatActivity {
 EditText reviewDescription;
 RatingBar simpleRatingBar;
+Button addCritBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +37,14 @@ RatingBar simpleRatingBar;
         Database db = new Database(getApplicationContext());
         Cursor foodDetails = db.getFood(foodID);
         foodDetails.moveToFirst();
-        String foodName = "Food name entered " + foodDetails.getString(0);
+        String storeFoodName = "Food name entered " + foodDetails.getString(0);
 
-        // Following variables used to insert data into the database
+        // The Following food name, rating bar, review description and add Critic btn are initialised with their corresponding ids in activity add review xml.
+        TextView foodNameValue = findViewById(R.id.foodName);
+        foodNameValue.setText(storeFoodName);
         reviewDescription = findViewById(R.id.reviewEditText);
         simpleRatingBar = findViewById(R.id.ratingBar);
+
 
     }
 }
