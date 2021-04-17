37f6b8f7913112f63d295e670d5c88a2d79e7c99
Sahil Ahmed <sahil.98@hotmail.com>
2021-04-17 14:37:17 +0100

    Added Fragment Manager to return Fragment.

--- app/src/main/java/com/example/myfoodcriticapp/ListRestaurantActivity.java
+++ app/src/main/java/com/example/myfoodcriticapp/ListRestaurantActivity.java
@@ -1,14 +1,31 @@
 package com.example.myfoodcriticapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class ListRestaurantActivity extends AppCompatActivity {
-
+Button backBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_restaurant);
+        LinearLayout container = findViewById(R.id.linearContainer);
+        /**
+         * @return the fragment manager that is associated with this activity. Fragment is then
+         * initialised with a beginTransaction method.
+         */
+        FragmentTransaction fragmentTransact = getSupportFragmentManager().beginTransaction();
+        fragmentTransact.add(R.id.fragment_container_view, FragmentActivity.class,null);
+        fragmentTransact.commit();
+
+
     }
 }
