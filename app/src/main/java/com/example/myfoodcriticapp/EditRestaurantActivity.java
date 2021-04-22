ea6bfa80aafd57e3a4f0bede314402b86882b8ea
Sahil Ahmed <sahil.98@hotmail.com>
2021-04-22 22:58:29 +0100

    Created new EditRestaurantActivity class and xml. Textviews and Buttons implemented to edit any records that has a restaurant, food or ratings.

---
+++ app/src/main/java/com/example/myfoodcriticapp/EditRestaurantActivity.java
@@ -0,0 +1,23 @@
+package com.example.myfoodcriticapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class EditRestaurantActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_restaurant);
+
+
+    }
+}
