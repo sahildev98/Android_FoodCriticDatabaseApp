9610f962637640a1c497c10de1c652d0adf07956
Sahil Ahmed <sahil.98@hotmail.com>
2021-05-06 00:52:44 +0100

    editButton is set with a new intent to start editRestaurantActivity.

--- app/src/main/java/com/example/myfoodcriticapp/ViewRestaurantActivity.java
+++ app/src/main/java/com/example/myfoodcriticapp/ViewRestaurantActivity.java
@@ -42,7 +42,7 @@ public class ViewRestaurantActivity extends AppCompatActivity {
         editButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(ViewRestaurantActivity.this,EditRestaurantActivity.class);
             }
         });
 
