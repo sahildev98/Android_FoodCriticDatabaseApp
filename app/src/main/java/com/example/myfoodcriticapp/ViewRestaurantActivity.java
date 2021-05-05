4917cc3bf8f305998ec91e1b9de29e7ddaf7eeb6
Sahil Ahmed <sahil.98@hotmail.com>
2021-05-05 18:08:26 +0100

    Created new ViewRestaurantActivity.java.

---
+++ app/src/main/java/com/example/myfoodcriticapp/ViewRestaurantActivity.java
@@ -0,0 +1,24 @@
+package com.example.myfoodcriticapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.net.IDN;
+
+public class ViewRestaurantActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_restaurant);
+
+    }
+}
