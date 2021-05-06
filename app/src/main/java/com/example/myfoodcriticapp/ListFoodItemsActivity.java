7ca15d7d221fabdbf08e352f11b5b326be5cb16a
Sahil Ahmed <sahil.98@hotmail.com>
2021-05-06 14:13:27 +0100

    long foodID created to get intent data from AddFoodActivity.java

--- app/src/main/java/com/example/myfoodcriticapp/ListFoodItemsActivity.java
+++ app/src/main/java/com/example/myfoodcriticapp/ListFoodItemsActivity.java
@@ -3,8 +3,13 @@ package com.example.myfoodcriticapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class ListFoodItemsActivity extends AppCompatActivity {
     private Database db;
@@ -24,5 +29,6 @@ public class ListFoodItemsActivity extends AppCompatActivity {
 
         //  // returns the application object context in order to return data
         db = new Database(getApplicationContext());
+        long foodID = getIntent().getLongExtra("restaurantID", 0);
     }
 }
