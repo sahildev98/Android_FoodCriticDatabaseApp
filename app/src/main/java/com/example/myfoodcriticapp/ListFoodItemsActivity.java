e5fceccea4bbdb2617bfd395a6876274beb2d8f1
Sahil Ahmed <sahil.98@hotmail.com>
2021-05-06 12:32:42 +0100

    Set Fragment container to display fragment. Implementation of java doc.

--- app/src/main/java/com/example/myfoodcriticapp/ListFoodItemsActivity.java
+++ app/src/main/java/com/example/myfoodcriticapp/ListFoodItemsActivity.java
@@ -1,6 +1,7 @@
 package com.example.myfoodcriticapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.widget.LinearLayout;
@@ -12,5 +13,13 @@ public class ListFoodItemsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_food_items);
         LinearLayout container = findViewById(R.id.linearContainer);
+
+        /**
+         * @return the fragment manager that is associated with this activity. Fragment is then
+         * initialised with a beginTransaction method.
+         */
+        FragmentTransaction fragmentTransact = getSupportFragmentManager().beginTransaction();
+        fragmentTransact.add(R.id.fragment_container_view, FragmentActivity.class,null);
+        fragmentTransact.commit();
     }
 }
