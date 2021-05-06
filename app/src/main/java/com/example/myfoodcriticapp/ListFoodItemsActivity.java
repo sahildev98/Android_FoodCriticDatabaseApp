5919d53bee08de78ba3539c349cf3ac12df05827
Sahil Ahmed <sahil.98@hotmail.com>
2021-05-06 12:31:39 +0100

    Declare LinearLayout and set ID that is in the xml for LinearLayout.

--- app/src/main/java/com/example/myfoodcriticapp/ListFoodItemsActivity.java
+++ app/src/main/java/com/example/myfoodcriticapp/ListFoodItemsActivity.java
@@ -3,6 +3,7 @@ package com.example.myfoodcriticapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class ListFoodItemsActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class ListFoodItemsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_food_items);
+        LinearLayout container = findViewById(R.id.linearContainer);
     }
 }
