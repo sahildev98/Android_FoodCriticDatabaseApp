35661c0e1d3b7b2763f37c471afc7157d0b095b8
Sahil Ahmed <sahil.98@hotmail.com>
2021-05-06 12:21:31 +0100

    Created new ListFoodItemsActivity with xml.

---
+++ app/src/main/java/com/example/myfoodcriticapp/ListFoodItemsActivity.java
@@ -0,0 +1,14 @@
+package com.example.myfoodcriticapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListFoodItemsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_food_items);
+    }
+}
