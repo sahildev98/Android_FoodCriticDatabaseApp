4b2d049ee6aa2d045e7e6befb7629439de40b2f0
Sahil Ahmed <sahil.98@hotmail.com>
2021-05-07 13:22:46 +0100

    Set field to not null to prevent empty records. Also implemented on update cascades.

--- app/src/main/java/com/example/myfoodcriticapp/DatabaseHelper.java
+++ app/src/main/java/com/example/myfoodcriticapp/DatabaseHelper.java
@@ -6,25 +6,25 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 
 public class DatabaseHelper extends SQLiteOpenHelper {
-    public DatabaseHelper (Context context) { super(context, "myFoodCriticDB.db", null, 4); }
+    public DatabaseHelper (Context context) { super(context, "myFoodCriticDB.db", null, 5); }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
         String restaurant_Query = "CREATE TABLE IF NOT EXISTS Restaurant (" +
                 "ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"+
-                "name text,"+
-                "address text,"+
-                "phone text"+
+                "name text NOT NULL,"+
+                "address text NOT NULL,"+
+                "phone text NOT NULL"+
                 ")";
                 db.execSQL(restaurant_Query);
 
         String food_Query = "CREATE TABLE IF NOT EXISTS Food (" +
                 "ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "+
                 "restaurantID int,"+
-                "name text,"+
-                "price real,"+
-                "description text,"+
-                "FOREIGN KEY (RestaurantID) REFERENCES restaurant(ID)"+
+                "name text NOT NULL,"+
+                "price real NOT NULL,"+
+                "description text NOT NULL,"+
+                "FOREIGN KEY (RestaurantID) REFERENCES restaurant(ID) ON UPDATE CASCADE"+
                 ")";
                 db.execSQL(food_Query);
 
@@ -32,9 +32,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         String ratings_Query = "CREATE TABLE IF NOT EXISTS Ratings (" +
                 "ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"+
                 "foodID int,"+
-                "ratings int,"+
-                "review text,"+
-                "FOREIGN KEY (foodID) REFERENCES Food(ID)"+
+                "ratings int NOT NULL,"+
+                "review text NOT NULL,"+
+                "FOREIGN KEY (foodID) REFERENCES Food(ID) ON UPDATE CASCADE"+
                 ")";
                 db.execSQL(ratings_Query);
     }
